196d335b143e53542c351e5d265127e684a485ef
dash4ast <124378154+dash4ast@users.noreply.github.com>
2025-05-22 09:26:42 +0200

    Create example.go

---
+++ example.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"crypto/aes"
+
+	a2 "crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+	"log"
+
+	"github.com/spacemonkeygo/openssl"
+)
+
+func encryptStringWithOpenSSL(text string) string {
+	// Static key for the purposes of static analysis
+	key := []byte("7E72436EDB3AE3EEBBC2B8DAD5D8575CA30AE3B8") // Key must be the correct size for the chosen cipher
+
+	//ruleid: aes-static-key
+	ctx, err := openssl.NewEncryptionCipherCtx(openssl.Cipher_AES_128_CBC, nil, key, nil)
+	if err != nil {
+		log.Fatalf("Failed to create new cipher context: %v", err)
+	}
+
+	//ruleid: aes-static-key
+	ctx2, err2 := openssl.NewEncryptionCipherCtx(openssl.Cipher_AES_256_CBC, nil, key, nil)
+	if err2 != nil {
+		log.Fatalf("Failed to create new cipher context: %v", err)
+	}
+
+	DoSomething(ctx2)
+
+	ciphertext, err := ctx.EncryptUpdate([]byte(text))
+	if err != nil {
+		log.Fatalf("EncryptUpdate failed: %v", err)
+	}
+	final, err := ctx.EncryptFinal()
+	if err != nil {
+		log.Fatalf("EncryptFinal failed: %v", err)
+	}
+	ciphertext = append(ciphertext, final...)
+
+	return base64.StdEncoding.EncodeToString(ciphertext)
+}
+
+func encryptStringWithAESStandardLib(text string) string {
+	// Static key for the purposes of static analysis
+	key := []byte("7E72436EDB3AE3EEBBC2B8DAD5D8575CA30AE3B8") // 16 bytes for AES-128, use 32 bytes for AES-256
+
+	// ruleid: aes-static-key
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Fatalf("Failed to create new cipher: %v", err)
+	}
+
+	// ruleid: aes-static-key
+	block2, err2 := a2.NewCipher(key)
+	if err2 != nil {
+		log.Fatalf("Failed to create new cipher: %v", err)
+	}
+	DoSomething(block2)
+
+	plaintext := []byte(text)
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		log.Fatal(err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return base64.StdEncoding.EncodeToString(ciphertext)
+}
+
+func main() {
+	encrypted := encryptStringWithAESStandardLib("Hello, World!")
+	log.Printf("Encrypted: %s", encrypted)
+}
